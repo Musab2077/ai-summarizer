67c72c75c633033408bffb5864dd71c3a5ee3fa7
Musab2077 <muhammadmusab2077@gmail.com>
2025-09-15 19:50:44 +0500

    fixing the text for size

--- frontend/src/components/BeforeSummarization.jsx
+++ frontend/src/components/BeforeSummarization.jsx
@@ -5,7 +5,7 @@ const BeforeSummarization = () => {
   return (
     <div className="place-content-center place-items-center">
       <div className="space-y-2">
-        <Steps stepsNum={"01."} stepDetail={"Upload your document (.txt, .docx or .pdf file) less than 5Mb on the left-hand side"} />
+        <Steps stepsNum={"01."} stepDetail={"Upload your document (.txt, .docx or .pdf file) less than 700Kb on the left-hand side"} />
         <Steps stepsNum={"02."} stepDetail={"Click the AI Summarize button and enjoy"} arrowRes={false} />
         {/* <Steps stepsNum={"03."} stepDetail={""} arrowRes={false} /> */}
       </div>
